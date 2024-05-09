45934a05926d036b1665a3df4f7b8ca4a3872291
Denis Palnitsky <palnitsky@gmail.com>
2024-05-09 21:59:38 +0200

    1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Instructions from "./components/Instructions";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Instructions />
+    </>
+  );
+}
+
+export default App;
