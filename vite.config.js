4bdc59cc1a865f39d221ceacd3ed1950d6e123bf
Denis Palnitsky <denis.palnitsky@zendesk.com>
2024-11-26 10:10:34 +0100

    fix deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import ViteYaml from '@modyfi/vite-plugin-yaml';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/korty-wroclawia/", // Set base to the repository name for GitHub Pages
+  base: "/", // Set base to the repository name for GitHub Pages
   plugins: [react(), ViteYaml()],
 });
